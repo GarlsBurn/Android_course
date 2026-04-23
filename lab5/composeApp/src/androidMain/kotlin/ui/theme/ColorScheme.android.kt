325b241ba52ed1c7fa808c5a75873115b3e515e0
Ilya Belikov <belikovilya0@gmail.com>
2026-04-23 20:55:16 +0300

    Выполнение лабораторной работы №5 - Запуск на мобильном устройстве. Системная тематика оформления

---
+++ lab5/composeApp/src/androidMain/kotlin/ui/theme/ColorScheme.android.kt
@@ -0,0 +1,23 @@
+package ui.theme
+
+import android.os.Build
+
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.ReadOnlyComposable
+import androidx.compose.ui.platform.LocalContext
+
+@Composable
+@ReadOnlyComposable
+actual fun getApplicationColorScheme(useDarkTheme: Boolean): ColorScheme {
+    val context = LocalContext.current
+    val colors = when {
+        (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) -> {
+            if (useDarkTheme) dynamicDarkColorScheme(context)
+            else dynamicLightColorScheme(context)
+        }
+        useDarkTheme -> darkColorScheme()
+        else -> lightColorScheme()
+    }
+    return colors
+}
